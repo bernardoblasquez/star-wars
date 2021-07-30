cbaa17789c8c98410ff6c5b96cd87000849e42cb
Bernardo <bernardo.bt@gmail.com>
2021-07-30 11:14:12 -0300

    feature: configurando acesso a SWAPI com fetch/then

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -6,17 +6,38 @@ import "./App.css";
 // https://swapi.dev/
 
 function App() {
+  
+	const [movies, setMovies] = useState([])
 
-  return (
-    <React.Fragment>
-      <section>
-        <button>Fetch Movies</button>
-      </section>
-      <section>
-        <MoviesList />
-      </section>
-    </React.Fragment>
-  );
-}
+	function fetchMoviesHandler() {
+		fetch("https://swapi.dev/api/films/")
+			.then((response) => {
+				return response.json();
+			})
+			.then((data) => {
+				
+				const transformedMovieData = data.results.map( movieData =>{
+					return {
+						id: movieData.episode_id,
+						title: movieData.title,
+						openingText: movieData.opening_crawl,
+						releaseDate: movieData.release_data
+					};
+				})
+				setMovies(transformedMovieData);
+			});
+	}
+
+	return (
+		<React.Fragment>
+		<section>
+			<button onClick={fetchMoviesHandler}>Fetch Movies</button>
+		</section>
+		<section>
+			<MoviesList movies={movies} />			
+		</section>
+		</React.Fragment>
+	);
+	}
 
 export default App;
